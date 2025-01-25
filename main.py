d84e65d0ba4b33ae8837c6fcb66e82d9921bb2e5
Vedant1607 <vedantsinha1607@gmail.com>
2025-01-25 23:30:30 +0530

    Get Top 100 Songs

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-URL = "https://www.billboard.com/charts/hot-100/"
-
 # Function to get date from user
 def dateInput():
     inputDate = input("which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
@@ -14,3 +12,12 @@ def dateInput():
     except ValueError:
         print("Invalid date format. Please use YYYY-MM-DD.")    
 date = dateInput()
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+website = f"https://www.billboard.com/charts/hot-100/{date.strftime("%Y-%m-%d")}"
+response = requests.get(url=website,headers=header)
+website_html = response.text
+
+soup = BeautifulSoup(website_html,"html.parser")
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
